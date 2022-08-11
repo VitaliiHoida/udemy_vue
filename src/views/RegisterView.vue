ed377ce1df60d1c259532cd16114494653dde03d
Vitalii Hoida <44941430+VitaliiHoida@users.noreply.github.com>
2022-08-11 20:17:54 +0300

    lesson 8

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -5,7 +5,7 @@
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">Sign Up</h1>
           <p class="text-xs-center">
-            <router-link to="{name: 'login'}">Need an account?</router-link>
+            <!--            <router-link to="{name: 'login'}">Need an account?</router-link>-->
           </p>
           VALIDATION ERRORS
           <form @submit.prevent="onSubmit">
@@ -18,7 +18,7 @@
             <fieldset class="form-group">
               <input type="password" class="form-control form-control-lg" placeholder="Password"/>
             </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right">Sign Up</button>
+            <button class="btn btn-lg btn-primary pull-xs-right" :disabled="isSubmitting">Sign Up</button>
           </form>
         </div>
       </div>
@@ -28,11 +28,16 @@
 
 <script>
 export default {
-name: 'RegisterView',
-  methods:{
-  onSubmit(){
-    console.log('Submitted form');
+  name: 'RegisterView',
+  computed: {
+    isSubmitting(){
+      return this.$store.state.auth.isSubmitting;
+    }
   },
+  methods: {
+    onSubmit() {
+      this.$store.commit('registerStart');
+    },
   }
 }
 </script>
